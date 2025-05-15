8d7574f651c81ee509c869c155a7ccefd74a3897
AntonKhodokaynen <khanton3@gmail.com>
2025-05-15 21:55:48 +0300

    Add files via upload

---
+++ lab89/DataAccess/NotesDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyNotes.Models;
+
+namespace MyNotes.DataAccess;
+
+public class NotesDbContext : DbContext
+{
+    public NotesDbContext(DbContextOptions<NotesDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Note> Notes => Set<Note>();
+}
+
